d36c8f9fb5aba4db59200fe9ab163380a93dff63
Natta Channel <love4nut@gmail.com>
2022-04-14 18:11:09 +0700

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const apps = {
+    owner : "asp-matrix.com",
+    description : "RESTful Web Services with Node.js and Express",
+    version : "1.0",
+    created : "2022-4-14"
+  }
+  res.status(200).json(apps)
 }
